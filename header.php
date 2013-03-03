2e4ea1303e49cc1259c2c6c99c9e8f4c6c013e47
James Glinsek <james.glinsek@rezstream.com>
2013-03-03 16:48:19 -0600

    Added Google Analytics script.

--- header.php
+++ header.php
@@ -72,7 +72,20 @@
 		if (isset($pagecustoms["portfolio_layout"])){$apex_portfoliolayout = $pagecustoms['portfolio_layout'];}else{$apex_portfoliolayout = "";}
 		if($apex_portfoliolayout==0){$foliocol = "portfolio4column"; }else{$foliocol = "portfolio2column";}
 	?>
-    
+
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-4993510-2']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 </head>
 
 <body <?php body_class(); ?>> 
